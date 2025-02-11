0b9014ed83c5fafef984824995f0b8f744448261
Andre Ricardo <andrericardo.developer@gmail.com>
2025-02-11 11:29:45 -0300

    first commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# autocomplete_widget_flutter
+
+A new Flutter project.
---
+++ android/app/src/main/kotlin/com/example/autocomplete_widget_flutter/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.autocomplete_widget_flutter
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
